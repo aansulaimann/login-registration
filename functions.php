471bb68a81962f88f12f15b839bf47cc0ad5c1dd
aansulaimann <aansulaiman92@gmail.com>
2021-01-25 17:51:06 +0700

    create registration page in gate.php and create functionality registration in functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,25 @@
 <?php
 
+$conn = mysqli_connect("localhost", "root", "", "simplelogin");
+
+
 function login($data) {
+    global $conn;
+
     $email = htmlspecialchars(trim($data['email']));
     $password = htmlspecialchars(trim($data['pass']));
 
-    if($email == "admin@apk.id" && $password == "admin123") {
-        return TRUE;
+    $query = "SELECT username, email, password FROM user WHERE email = '$email'";
+    $res = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($res) === 1) {
+        $fetch = mysqli_fetch_assoc($res);
+        if(password_verify($password, $fetch['password'])) {
+            // return true;
+            return mysqli_affected_rows($conn);
+        }
     }
+    exit;
 }
 
 function logout() {
@@ -17,4 +30,35 @@ function logout() {
 
     header("Location: http://localhost/simple-login/gate.php");
     exit;
+}
+
+function reg($data) {
+    global $conn;
+
+    $username = mysqli_real_escape_string($conn, htmlspecialchars(trim($data['username'])));
+    $email = mysqli_real_escape_string($conn, htmlspecialchars(trim($data['email'])));
+    $pass = mysqli_real_escape_string($conn, htmlspecialchars(trim($data['pass'])));
+    $pass2 = mysqli_real_escape_string($conn, htmlspecialchars(trim($data['confirmPass'])));
+
+    $res = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+
+    if(mysqli_fetch_assoc($res)) {
+        echo "<script> 
+                alert('Username sudah ada!');
+            </script>";
+        return false;
+    }
+
+    if($pass !== $pass2) {
+        echo "<script> 
+                alert('Password tidak sesuai!');
+            </script>";
+        return false;
+    }
+
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+   
+    $query = "INSERT INTO user (Id, username, email, password) VALUES('', '$username', '$email', '$pass')";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
 }
--- gate.php
+++ gate.php
@@ -5,12 +5,25 @@
     }
 
     if(isset($_POST['btn_login'])) {
-        if(login($_POST) === TRUE) {
+        if(login($_POST) > 0) {
             $_SESSION['login'] = true;
             header('Location: home.php');
             exit;
         } else {
             echo 'username / password salah';
+            exit;
+        }
+    }
+
+    if(isset($_POST['reg'])) {
+        if(reg($_POST) > 0) {
+            echo $err = '<div class="alert alert-primary" role="alert">
+                        Success Sign Up.
+                    </div>';
+        } else {
+            echo $err = '<div class="alert alert-danger" role="alert">
+                        Invalid Sign Up.
+                    </div>';
         }
     }
 ?>
@@ -34,20 +47,58 @@
                         <p class="text-primary">If you administrator please Login</p>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" autocomplete="off">
                             </div>
                             <div class="mb-3">
                                 <label for="pass" class="form-label">Password</label>
                                 <input type="password" class="form-control" id="pass" name="pass">
                             </div>
-                            <button type="submit" class="me-2 btn btn-primary shadow mt-4" name="btn_login">Log in</button>
-                            <button type="button" class="me-2 btn btn-outline-light text-body shadow mt-4">Registration</button>
+                            <button type="submit" class="me-2 btn btn-primary shadow mt-4" name="btn_login">Sign in</button>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-outline-light text-body ms-2 mt-4 shadow" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Sign up
+                            </button>
                         </form>
                     </div>
                 </div>
             </div>
         </section>
     </main>
+
+    <!-- Modal -->
+    <form action="" method="post">
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Sign Up</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="john" name="username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleFormControlInput1" name="pass">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Confitm Password</label>
+                        <input type="password" class="form-control" id="exampleFormControlInput1" name="confirmPass">
+                    </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary" name="reg">Create account</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </form>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>    
 </body>
